b3d6580c439a1447a71f4d9e4ca97ffd5ff62af2
PraveenNeelamegam <50565637+PraveenNeelamegam@users.noreply.github.com>
2019-05-13 10:26:29 +0530

    updating existing file

--- WelcomeService/src/test/java/com/deloitte/WelcomeServiceApplicationTests.java
+++ WelcomeService/src/test/java/com/deloitte/WelcomeServiceApplicationTests.java
@@ -1,16 +1,26 @@
 package com.deloitte;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.deloitte.controller.WelcomeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WelcomeServiceApplicationTests {
 
+	@Autowired
+	WelcomeController welcomeController;
+	
 	@Test
-	public void contextLoads() {
+	public void WelcomeControllerTest() {
+		assertEquals("Welcome To Deloitte From Port null",welcomeController.welcome());
+		
 	}
 
 }
